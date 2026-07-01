925ccbea950cbad8e53b74c4062924d753e60a10
IsaacDMD <isaacdjimadjo830@gmail.com>
2026-07-01 10:36:56 +0000

    Model : Fin de la création des models nécéssaires

---
+++ backend/app/enums/favoris_type.py
@@ -0,0 +1,9 @@
+class FavorisTypeEnum:
+    """
+    Enumeration pour les differents types de favoris 
+    """
+    ANNONCE = "annonce"
+    EVENEMENT = "evenement"
+    MEDIA = "media"
+    CHANT = "chant"
+    ARTICLE = "article"
