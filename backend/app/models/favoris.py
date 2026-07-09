aded6f01751ccb07ff6abf861cf1160b8b0c80ff
IsaacDMD <isaacdjimadjo830@gmail.com>
2026-07-09 10:16:16 +0000

    Ajout de la contrainte d'unicité sur favoris

--- backend/app/models/favoris.py
+++ backend/app/models/favoris.py
@@ -1,10 +1,13 @@
-from sqlalchemy import Column, Enum, ForeignKey, Integer, TIMESTAMP
+from sqlalchemy import Column, Enum, ForeignKey, Integer, TIMESTAMP, UniqueConstraint
 from sqlalchemy.sql import func
 from app.database import Base
 from app.enums.favoris_type import FavorisTypeEnum
 
 class Favoris(Base):
     __tablename__ = "favoris"
+    __table_args__ = (
+        UniqueConstraint("user_id", "type_favoris", "item_id", name="uq_favoris_user_type_item"),
+    )
 
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
